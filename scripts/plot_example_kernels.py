64656c7ad6c7ef2e1c83be09cfa12d8e255e8765
Sebastian Callh <sebastian.callh@gmail.com>
2021-01-31 16:13:02 +0100

    Add example kernel plots

---
+++ scripts/plot_example_kernels.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+import torch
+import gpytorch as gp
+
+from smk.plots import save_plot
+
+
+def plot_kernel(kernel, ax, xx=torch.linspace(-2, 2, 1000), col=sns.color_palette()[0]):
+    x0 = torch.zeros(xx.size(0))
+    ax.plot(xx.numpy(), np.diag(kernel(xx, x0).numpy()), lw=3, color=col)
+
+
+fig, axs = plt.subplots(1, 3, figsize=(16, 6))
+kernels = [
+    gp.kernels.RBFKernel(),
+    gp.kernels.CosineKernel(),
+    gp.kernels.MaternKernel(1 / 2),
+]
+colors = [sns.color_palette()[i] for i in range(3)]
+
+titles = ["RBF", "Cosine", "Matérn 1/2"]
+n = 100
+x0 = torch.zeros(n)
+xx = torch.linspace(-2, 2, n)
+for k, title, col, ax in zip(kernels, titles, colors, axs):
+    plot_kernel(kernel=k, ax=ax, col=col)
+    ax.set_title(title)
+    ax.set_ylabel("Similarity")
+    ax.set_xlabel("Distance")
+
+save_plot(fig, "example_kernels")
